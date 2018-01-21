5a6f02787077e2fde9e25d603f06a5bd3805e5d6
Avi Warner <aviwarner@gmail.com>
2018-01-20 21:27:20 -0800

    crud assignment work

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,12 @@
 class Post < ApplicationRecord
   has_many :comments
+
+  def self.censor
+    Post.all.each { | post |
+      if post.id % 5 == 0 || post.id == 1
+        post.update_attributes(:title => 'SPAM')
+      end
+    }
+  end
+
 end
