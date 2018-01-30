dd5dceb70f786b5ab26fabc230ec9cde2de3a8b2
Avi Warner <aviwarner@gmail.com>
2018-01-29 21:58:09 -0800

    Topics checkpoint. Finally done

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,20 +8,29 @@
 
 require 'random_data'
 
+15.times do
+  Topic.create!(
+    name:         RandomData.random_sentence,
+    description:  RandomData.random_paragraph
+  )
+end
+topics = Topic.all
+
 50.times do
   Post.create!(
-    title: RandomData.random_sentence,
-    body: RandomData.random_paragraph
+    topic:        topics.sample,
+    title:        RandomData.random_sentence,
+    body:         RandomData.random_paragraph
   )
 end
 posts = Post.all
 
 100.times do
   Comment.create!(
-    post: posts.sample,
-    body: RandomData.random_paragraph
+    post:         posts.sample,
+    body:         RandomData.random_paragraph
   )
 end
 
 puts "Seed finished"
-puts "#{Post.count} posts created and #{Comment.count} comments created."
+puts "#{Topic.count} topics created, #{Post.count} posts created and #{Comment.count} comments created."
