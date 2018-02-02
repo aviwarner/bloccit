c935081f318e19382d3fb2dd7c6e5f438f290d01
Avi Warner <aviwarner@gmail.com>
2018-02-01 21:45:38 -0800

    users assignment work

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -22,6 +22,24 @@ RSpec.describe User, type: :model do
     it "should have name and email attributes" do
       expect(user).to have_attributes(name: "Bloccit User", email:"user@bloccit.com")
     end
+
+    it "should correctly capitalize a users name when uppercase" do
+      user.name = "BLOC USER"
+      user.save
+      expect(user.name).to eq("Bloc User")
+    end
+
+    it "should correctly capitalize a users name when lowercase" do
+      user.name = "bloc user also"
+      user.save
+      expect(user.name).to eq("Bloc User Also")
+    end
+
+    it "should not change correctly capitalized users name" do
+      user.name = "Last User"
+      user.save
+      expect(user.name).to eq("Last User")
+    end
   end
 
   describe "invalid user" do
@@ -36,4 +54,18 @@ RSpec.describe User, type: :model do
       expect(user_with_invalid_email).to_not be_valid
     end
   end
+
+  # describe "name capitalization" do
+  #   let(:user_with_uncapitalized_name) { User.new(name:"joe sample", email:"user@bloccit.com") }
+  #   let(:user_with_allcapitalized_name) { User.new(name:"JOE SAMPLE", email:"user@bloccit.com") }
+  #
+  #   it "should convert lowercase name to capitalized name" do
+  #     expect(user_with_uncapitalized_name).to have_attributes(name: "Joe Sample", email:"user@bloccit.com")
+  #   end
+  #
+  #   it "should convert all uppercase name to capitalized name" do
+  #     expect(user_with_allcapitalized_name).to have_attributes(name: "Joe Sample", email:"user@bloccit.com")
+  #   end
+  #
+  # end
 end
