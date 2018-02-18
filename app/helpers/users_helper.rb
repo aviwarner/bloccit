79509ffd76ffbe542bc616c6abd584a19617478a
Avi Warner <aviwarner@gmail.com>
2018-02-17 20:10:00 -0800

    Profiles Assignment work

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,13 @@
 module UsersHelper
+  def user_has_posts?(user)
+    user.posts.length != 0
+  end
+
+  def user_has_comments?(user)
+    user.comments.length != 0
+  end
+
+  def user_has_favorites?(user)
+    user.favorites.length != 0
+  end
 end
